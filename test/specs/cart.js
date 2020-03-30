49875eba2bed6d15e73cbe010dfaf7caeb195146
mdsauce <max.dobeck@saucelabs.com>
2020-03-30 11:21:34 -0700

    updated specs to include wdio-expect only.

--- test/specs/cart.js
+++ test/specs/cart.js
@@ -7,14 +7,14 @@ describe('Cart', () => {
     it('should add the bike light', () => {
         LoginPage.login('standard_user')
         browser.pause(10000)
-        expect(InventoryPage.itemName(2).getText()).toHaveTextContaining("Bike Light")
+        expect(InventoryPage.itemName(2)).toHaveTextContaining("Bike Light")
         InventoryPage.item(2).$('.pricebar').$('button').click()
         expect(InventoryPage.item(2).$('.pricebar').$('button')).toHaveTextContaining("REMOVE")
     })
 
     it('should add the backpack', () => {
         browser.pause(10000)
-        expect(InventoryPage.itemName(1).getText()).toHaveTextContaining("Backpack")
+        expect(InventoryPage.itemName(1)).toHaveTextContaining("Backpack")
         InventoryPage.item(1).$('.pricebar').$('button').click()
         expect(InventoryPage.item(1).$('.pricebar').$('button')).toHaveTextContaining("REMOVE")
     })
