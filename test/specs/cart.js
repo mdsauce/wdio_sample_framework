e0d9cbb7f4ea70ceaf38881ae0aca17c99063ea9
mdsauce <max.dobeck@saucelabs.com>
2020-03-30 10:43:29 -0700

    updated to remove chai and add wdio-expect.

--- test/specs/cart.js
+++ test/specs/cart.js
@@ -1,5 +1,4 @@
 // inventory_default.js
-var expect = require('chai').expect
 import InventoryPage from '../../pages/inventory.page'
 import LoginPage from '../../pages/login.page'
 
@@ -8,16 +7,16 @@ describe('Cart', () => {
     it('should add the bike light', () => {
         LoginPage.login('standard_user')
         browser.pause(10000)
-        expect(InventoryPage.itemName(2).getText()).to.contain("Bike Light")
+        expect(InventoryPage.itemName(2).getText()).toHaveTextContaining("Bike Light")
         InventoryPage.item(2).$('.pricebar').$('button').click()
-        expect(InventoryPage.item(2).$('.pricebar').$('button').getText()).to.contain("REMOVE")
+        expect(InventoryPage.item(2).$('.pricebar').$('button')).toHaveTextContaining("REMOVE")
     })
 
     it('should add the backpack', () => {
         browser.pause(10000)
-        expect(InventoryPage.itemName(1).getText()).to.contain("Backpack")
+        expect(InventoryPage.itemName(1).getText()).toHaveTextContaining("Backpack")
         InventoryPage.item(1).$('.pricebar').$('button').click()
-        expect(InventoryPage.item(1).$('.pricebar').$('button').getText()).to.contain("REMOVE")
+        expect(InventoryPage.item(1).$('.pricebar').$('button')).toHaveTextContaining("REMOVE")
     })
 
     // it ('should have the cart badge', () => {
